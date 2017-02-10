1723b100c7349d2a5bf555c9c68c8a9c65669250
Lester Xue <lester.xue@parivedasolutions.com>
2017-02-10 11:28:22 -0500

    Python script to load PostGres data to ElasticSearch.

---
+++ postgresToElasticSearch.py
@@ -0,0 +1,39 @@
+import psycopg2
+from elasticsearch import Elasticsearch
+import json
+
+hostname = 'localhost'
+username = 'postgres'
+password = 'password'
+database = 'vitalsigns_staging'
+
+es = Elasticsearch()
+
+# Simple routine to run a query on a database and print the results:
+def doQuery( conn ) :
+    cur = conn.cursor()
+
+    viewName = "curation__agric_byprod"
+    cur.execute( 'SELECT uuid AS id, row_to_json(' + viewName + ') AS body FROM ' + viewName)
+
+    for row in cur.fetchall():
+
+    	if row[1]["latitude"] != "undefined":
+            row[1]["location"] = [
+	        	row[1]["longitude"],
+	        	row[1]["latitude"]
+        	]
+        else:
+        	del row[1]["latitude"]
+        	del row[1]["longitude"]
+
+        res = es.index(
+        	index=viewName, doc_type=viewName, id=row[0], body=row[1]
+        	)
+        status="Row ID: " + row[0] + " | Created: " + str(res['created']) + " | Result: " + res['result']
+        print(status)
+
+
+myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database, port=5432 )
+doQuery( myConnection )
+myConnection.close()
