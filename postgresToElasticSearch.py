b254133b38a5c6b81dc7cfd069c91b32d0e66454
Matt Cooper <mcooper@conservation.org>
2017-04-11 13:57:30 -0400

    just secB

--- postgresToElasticSearch.py
+++ postgresToElasticSearch.py
@@ -13,25 +13,25 @@ es = Elasticsearch()
 def doQuery( conn ) :
     cur = conn.cursor()
 
-    viewName = "curation__agric_byprod"
-    cur.execute( 'SELECT uuid AS id, row_to_json(' + viewName + ') AS body FROM ' + viewName)
+    #viewName = "curation__agric_byprod"
+    #cur.execute( 'SELECT uuid AS id, row_to_json(' + viewName + ') AS body FROM ' + viewName)
 
-    for row in cur.fetchall():
+    #for row in cur.fetchall():
 
-    	if row[1]["latitude"] != "undefined":
-            row[1]["location"] = [
-	        	row[1]["longitude"],
-	        	row[1]["latitude"]
-        	]
-        else:
-        	del row[1]["latitude"]
-        	del row[1]["longitude"]
+    #	if row[1]["latitude"] != "undefined":
+    #        row[1]["location"] = [
+	#        	row[1]["longitude"],
+	#        	row[1]["latitude"]
+    #    	]
+    #    else:
+    #    	del row[1]["latitude"]
+    #    	del row[1]["longitude"]
 
-        res = es.index(
-        	index=viewName, doc_type=viewName, id=row[0], body=row[1]
-        	)
-        status="Row ID: " + row[0] + " | Created: " + str(res['created']) + " | Result: " + res['result']
-        print(status)
+	#	res = es.index(
+    #    	index=viewName, doc_type=viewName, id=row[0], body=row[1]
+    #    	)
+    #    status="Row ID: " + row[0] + " | Created: " + str(res['created']) + " | Result: " + res['result']
+    #    print(status)
 		
 		
 	viewName = "curation__household_secB"
