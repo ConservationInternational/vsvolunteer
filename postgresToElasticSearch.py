9b18264bb9678af9d4fc835f7708da831b68eae7
Matt Cooper <mcooper@conservation.org>
2017-04-11 13:44:37 -0400

    Household sec B

--- postgresToElasticSearch.py
+++ postgresToElasticSearch.py
@@ -27,6 +27,27 @@ def doQuery( conn ) :
         	del row[1]["latitude"]
         	del row[1]["longitude"]
 
+        #res = es.index(
+        #	index=viewName, doc_type=viewName, id=row[0], body=row[1]
+        #	)
+        status="Row ID: " + row[0] + " | Created: " + str(res['created']) + " | Result: " + res['result']
+        print(status)
+		
+		
+	viewName = "curation__household_secB"
+    cur.execute( 'SELECT uuid AS id, row_to_json(' + viewName + ') AS body FROM ' + viewName)
+
+    for row in cur.fetchall():
+
+    	if row[1]["latitude"] != "undefined":
+            row[1]["location"] = [
+	        	row[1]["longitude"],
+	        	row[1]["latitude"]
+        	]
+        else:
+        	del row[1]["latitude"]
+        	del row[1]["longitude"]
+
         res = es.index(
         	index=viewName, doc_type=viewName, id=row[0], body=row[1]
         	)
